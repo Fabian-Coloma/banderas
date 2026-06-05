b5ed035c46589d66516535c08a4d78d661078f8f
fabiancoloma <jcolomaatoche7@gmail.com>
2026-06-05 09:31:00 -0500

    codigoparaqueseveanlasbanderas

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,1 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+import { defineConfig } from 'vite'import tailwindcss from '@tailwindcss/vite'import { resolve } from 'path'import copy from 'rollup-plugin-copy' // <-- Importa el pluginexport default defineConfig({   plugins: [     tailwindcss(),     copy({       targets: [         { src: 'data.json', dest: 'dist' } // <-- Le dice a Vite: "Copia data.json a la carpeta final dist"      ],       hook: 'writeBundle' // Hace que se copie justo al terminar la compilación    })   ],   build: {     rollupOptions: {       input: {         main: resolve(__dirname, 'index.html')       }     }   } })
